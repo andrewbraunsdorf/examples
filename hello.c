570d6d00dd49957059a19c64e2dc9afd5c396844
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-04-26 01:13:04 +0000

    used int base division

--- hello.c
+++ hello.c
@@ -1,13 +1,23 @@
 #include <cs50.h>
+#include <math.h>
 #include <stdio.h>
 
 int main(void)
 {
-    string firstname = get_string("What is your first name? ");
-    string lastname = get_string("What is your last name? ");
-    printf("Hello Mr/Ms %s, %s\n", lastname, firstname);
+    // string firstname = get_string("What is your first name? ");
+    // string lastname = get_string("What is your last name? ");
+    // printf("Hello Mr/Ms %s, %s\n", lastname, firstname);
 
+    // int myNum = 36;
+    int  myFloat = round(3.87 * 100);
+    int other = round(1.52*100);
+    int total = myFloat + other;
+    int cents = total % 100;
+    int dollars = total /100;
+    printf("adding the numbers: %i.%i\n", dollars, cents);
 
+    float division = (float)78/15;
+    printf("the division: %.2f\n", division);
 
 }
 
